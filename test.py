8ce428fa25784fa77ed95f0e51de578b219fb439
Moises Casaverde <moises.casaverde@gmail.com>
2021-08-05 17:40:26 -0500

    Orders by emotions implemented

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+'''
+Sistema de ejecución de comandos por medio de gestos y emociones a través de la captura de
+imágenes del rostro utilizando WebCam, Python, Keras y OpenCV
+
+'''
+# Importando las librerias necesarias
 from keras.models import load_model
 from time import sleep
 from keras.preprocessing.image import img_to_array
@@ -6,27 +12,35 @@ import cv2
 import numpy as np
 import webbrowser
 
-
+# Cargando el archivo xml y h5
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades +'./haarcascade_frontalface_default.xml')
 classifier =load_model('./Emotion_Detection.h5')
 
+# Incializando las etiquetas que vamos a usar para las emociones
 class_labels = ['Angry','Happy','Neutral','Sad','Surprise']
 
+# Iniciando la camara web con CV2
 cap = cv2.VideoCapture(0)
 
+# Inicializando los contadores de las emociones
 langry = 0
 lsad = 0
 lneutral = 0
 lsurprise = 0
 lhappy = 0
 
+# Contador de frames sacados
+counter = 0
+
+TIMER = 100
 while True:
-    # Grab a single frame of video
+    # Toma solo un frame del video
     ret, frame = cap.read()
     labels = []
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
 
+    # Proceso de analisis y prediccion de la imagen
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h,x:x+w]
@@ -38,13 +52,13 @@ while True:
             roi = img_to_array(roi)
             roi = np.expand_dims(roi,axis=0)
 
-        # make a prediction on the ROI, then lookup the class
+        # Hace una prediccion en el ROI
 
             preds = classifier.predict(roi)[0]
-            print("\nprediction = ",preds)
+            #print("\nprediction = ",preds)
             label=class_labels[preds.argmax()]
-            print("\nprediction max = ",preds.argmax())
-            print("\nlabel = ",label)
+            #print("\nprediction max = ",preds.argmax())
+            #print("\nlabel = ",label)
             if label == "Angry":
                 langry += 1
             elif label == "Happy":
@@ -61,23 +75,48 @@ while True:
             cv2.putText(frame,'No Face Found',(20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
         print("\n\n")
     cv2.imshow('Emotion Detector',frame)
+    counter += 1
+    if counter >= TIMER:
+        res = max(langry, lsad, lsurprise, lhappy, lneutral)
+
+        if res == lneutral:
+            webbrowser.open("https://www.youtube.com/watch?v=AUhOgfsDEmE")
+        elif res == lsurprise:
+            webbrowser.open("https://www.youtube.com/watch?v=ZA9kag0UllY")
+        elif res == langry:
+            webbrowser.open("https://www.youtube.com/watch?v=DNrnDx-KZUY")
+        elif res == lsad:
+            webbrowser.open("https://www.friv5online.com/es")
+        counter = 0
+        langry = 0
+        lsad = 0
+        lneutral = 0
+        lsurprise = 0
+        lhappy = 0
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
 
-res = max(langry,lsad, lsurprise, lhappy, lneutral);
 total = langry + lsad + lsurprise + lhappy + lneutral
-print("Estuviste " + str((langry*100)/total) + "% del tiempo enojado.")
-print("Estuviste " + str((lsad*100)/total) + "% del tiempo triste.")
-print("Estuviste " + str((lsurprise*100)/total) + "% del tiempo sorprendido.")
-print("Estuviste " + str((lhappy*100)/total) + "% del tiempo feliz.")
-print("Estuviste " + str((lneutral*100)/total) + "% del tiempo sin alguna emocion reconocible.")
 
+print("Estuviste " + str(round((langry*100)/total,2)) + " % del tiempo enojado.")
+print("Estuviste " + str(round((lsad*100)/total,2)) + " % del tiempo triste.")
+print("Estuviste " + str(round((lsurprise*100)/total,2)) + " % del tiempo sorprendido.")
+print("Estuviste " + str(round((lhappy*100)/total,2)) + " % del tiempo feliz.")
+print("Estuviste " + str(round((lneutral*100)/total,2)) + " % del tiempo sin alguna emocion reconocible.")
+'''
 if res == lneutral:
     webbrowser.open("https://www.youtube.com/watch?v=AUhOgfsDEmE")
-
+elif res == lsurprise:
+    webbrowser.open("https://www.youtube.com/watch?v=ZA9kag0UllY")
+elif res == langry:
+    webbrowser.open("https://www.youtube.com/watch?v=DNrnDx-KZUY")
+elif res == lsad:
+    webbrowser.open("https://www.friv5online.com/es")
+'''
 
 
 
